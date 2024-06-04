6474a41d202da72b891f3596ecd25768ca1531f6
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-06-04 16:25:15 -0400

    .

--- frontend/src/pages/benefits/[...id].tsx
+++ frontend/src/pages/benefits/[...id].tsx
@@ -11,7 +11,6 @@ import {
 } from "@/lib/utils";
 
 import AudioPlayer from "@/components/audio-player";
-import Navbar from "@/components/navbar";
 import ShareButton from "@/components/share-button";
 import Layout from "@/components/layout";
 
@@ -33,10 +32,6 @@ export default function Page() {
     "@type": "",
   };
 
-  if (isPending) {
-    return "Loading...";
-  }
-
   if (error) {
     return error.message;
   }
@@ -44,75 +39,92 @@ export default function Page() {
   return (
     <Layout>
       <Head>
-        <title>{data.title} - Garden of Ilm</title>
         <meta charSet="utf-8" />
-        <meta name="description" content={data?.content} />
-        <meta name="keywords" content={data.title} />
 
-        <meta property="og:title" content={`${data.title} - Garden of Ilm`} />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content={`https://gardenofilm.com/benefits/${id?.[0]}/${id?.[1]}`}
-        />
-        <meta property="og:image" content="/logo.png" />
+        {isPending && <title>Garden of Ilm</title>}
 
-        <meta name="twitter:card" content="summary" />
-        <meta
-          property="twitter:title"
-          content={`${data.title} - Garden of Ilm`}
-        />
+        {!isPending && !error && (
+          <>
+            <title>{data.title} - Garden of Ilm</title>
 
-        <script type="application/ld+json">
-          {JSON.stringify(structuredData)}
-        </script>
+            <meta name="description" content={data?.content} />
+            <meta name="keywords" content={data.title} />
+
+            <meta
+              property="og:title"
+              content={`${data.title} - Garden of Ilm`}
+            />
+            <meta property="og:type" content="website" />
+            <meta
+              property="og:url"
+              content={`https://gardenofilm.com/benefits/${id?.[0]}/${id?.[1]}`}
+            />
+            <meta property="og:image" content="/logo.png" />
+
+            <meta name="twitter:card" content="summary" />
+            <meta
+              property="twitter:title"
+              content={`${data.title} - Garden of Ilm`}
+            />
+
+            <script type="application/ld+json">
+              {JSON.stringify(structuredData)}
+            </script>
+          </>
+        )}
       </Head>
 
       <div className="border-t border-neutral-300 bg-[#f9fbfa] pb-12 pt-4 md:pb-8 md:pt-0">
         <div className="mx-auto max-w-4xl px-[16px] py-0 md:px-[72px] md:pb-[50px] md:pt-3">
-          <h1 className="mt-[12px] text-[18px] font-semibold text-[#465c3e] md:mt-[26px] md:text-[24px]">
-            {data.title}
-          </h1>
+          {isPending && "Loading..."}
 
-          <div className="mt-3 flex items-center space-x-2 text-sm md:text-base">
-            <div className="text-neutral-500">
-              Uploaded {formatDate(data.createdAt)}
-            </div>
-            <div className="select-none text-neutral-300">|</div>
-            <div className="text-neutral-500">{data.views} Views</div>
-            <div className="grow"></div>
-            <div>
-              <ShareButton />
-            </div>
-          </div>
+          {!isPending && !error && (
+            <>
+              <h1 className="mt-[12px] text-[18px] font-semibold text-[#465c3e] md:mt-[26px] md:text-[24px]">
+                {data.title}
+              </h1>
 
-          {data?.audioUrl && (
-            <AudioPlayer
-              url={convertDropboxLinkToAudioSrcLink(data.audioUrl)}
-            />
-          )}
-
-          <div className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
-            {data.author}
-          </div>
-          <div className="mt-[15px] whitespace-pre-wrap text-[15px] leading-7 md:text-[18px] md:leading-10">
-            {data.content}
-          </div>
-          {data.additionalReferences && (
-            <div className="mt-[20px]">
-              <p className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
-                Additional references
-              </p>
-              <div className="mt-[12px] rounded-[12px] border-[1px] border-[#0000004d] bg-white px-[16px] py-[16px] text-[14px] md:px-[24px] md:py-[16px] md:text-[16px]">
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: formatAdditionalReferences(
-                      data?.additionalReferences,
-                    ),
-                  }}
-                />
+              <div className="mt-3 flex items-center space-x-2 text-sm md:text-base">
+                <div className="text-neutral-500">
+                  Uploaded {formatDate(data.createdAt)}
+                </div>
+                <div className="select-none text-neutral-300">|</div>
+                <div className="text-neutral-500">{data.views} Views</div>
+                <div className="grow"></div>
+                <div>
+                  <ShareButton />
+                </div>
               </div>
-            </div>
+
+              {data?.audioUrl && (
+                <AudioPlayer
+                  url={convertDropboxLinkToAudioSrcLink(data.audioUrl)}
+                />
+              )}
+
+              <div className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
+                {data.author}
+              </div>
+              <div className="mt-[15px] whitespace-pre-wrap text-[15px] leading-7 md:text-[18px] md:leading-10">
+                {data.content}
+              </div>
+              {data.additionalReferences && (
+                <div className="mt-[20px]">
+                  <p className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
+                    Additional references
+                  </p>
+                  <div className="mt-[12px] rounded-[12px] border-[1px] border-[#0000004d] bg-white px-[16px] py-[16px] text-[14px] md:px-[24px] md:py-[16px] md:text-[16px]">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: formatAdditionalReferences(
+                          data?.additionalReferences,
+                        ),
+                      }}
+                    />
+                  </div>
+                </div>
+              )}
+            </>
           )}
         </div>
       </div>
--- frontend/src/pages/fatwas/[...id].tsx
+++ frontend/src/pages/fatwas/[...id].tsx
@@ -26,10 +26,6 @@ export default function Page() {
     },
   });
 
-  if (isPending) {
-    return "Loading...";
-  }
-
   if (error) {
     return error.message;
   }
@@ -37,81 +33,98 @@ export default function Page() {
   return (
     <Layout>
       <Head>
-        <title>{data.title} - Garden of Ilm</title>
-
         <meta charSet="utf-8" />
-        <meta name="description" content={data.reply} />
-        <meta name="keywords" content={data.title} />
 
-        <meta property="og:title" content={`${data.title} - Garden of Ilm`} />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content={`https://gardenofilm.com/fatwas/${id?.[0]}/${id?.[1]}`}
-        />
-        <meta property="og:image" content="/logo.png" />
+        {isPending && <title>Garden of Ilm</title>}
 
-        <meta name="twitter:card" content="summary" />
-        <meta
-          property="twitter:title"
-          content={`${data.title} - Garden of Ilm`}
-        />
+        {!isPending && !error && (
+          <>
+            <title>{data.title} - Garden of Ilm</title>
+
+            <meta name="description" content={data?.reply} />
+            <meta name="keywords" content={data.title} />
+
+            <meta
+              property="og:title"
+              content={`${data.title} - Garden of Ilm`}
+            />
+            <meta property="og:type" content="website" />
+            <meta
+              property="og:url"
+              content={`https://gardenofilm.com/fatwas/${id?.[0]}/${id?.[1]}`}
+            />
+            <meta property="og:image" content="/logo.png" />
+
+            <meta name="twitter:card" content="summary" />
+            <meta
+              property="twitter:title"
+              content={`${data.title} - Garden of Ilm`}
+            />
+          </>
+        )}
       </Head>
 
       <div className="border-t border-neutral-300 bg-[#f9fbfa] pb-12 pt-4 md:pb-8 md:pt-0">
         <div className="mx-auto max-w-4xl px-[16px] py-0 md:px-[72px] md:pb-[50px] md:pt-3">
-          <h1 className="mt-[12px] text-[18px] font-semibold text-[#465c3e] md:mt-[26px] md:text-[24px]">
-            {data.title}
-          </h1>
+          {isPending && "Loading..."}
 
-          <div className="mt-3 flex items-center space-x-2 text-sm md:text-base">
-            <div className="text-neutral-500">
-              Uploaded {formatDate(data.createdAt)}
-            </div>
-            <div className="select-none text-neutral-300">|</div>
-            <div className="text-neutral-500">{data.views} Views</div>
-            <div className="grow"></div>
-            <div>
-              <ShareButton />
-            </div>
-          </div>
+          {!isPending && !error && (
+            <>
+              <h1 className="mt-[12px] text-[18px] font-semibold text-[#465c3e] md:mt-[26px] md:text-[24px]">
+                {data.title}
+              </h1>
 
-          <div className="mt-[20px] rounded-xl border-4 border-[#c7dfbd] bg-[#c7dfbd] md:mt-[42px]">
-            <div className="px-2 py-1.5 pb-2 text-sm font-bold uppercase tracking-[3.5px] text-[#5f7d54] md:text-lg">
-              Question
-            </div>
-            <h2 className="rounded-b-xl bg-[#fff] px-4 py-3 text-[14px] font-medium leading-[24px] text-[#19372B] md:text-[20px] md:leading-[32px]">
-              {data.question}
-            </h2>
-          </div>
-
-          {data?.audios.map((audio: Audio) => (
-            <AudioPlayer key={audio._id} url={audio.url} />
-          ))}
-
-          <div className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
-            {data.author}
-          </div>
-
-          <div className="mt-[15px] whitespace-pre-wrap text-[15px] leading-7 md:text-[18px] md:leading-10">
-            {data.reply}
-          </div>
-
-          {data.additionalReferences && (
-            <div className="mt-[20px]">
-              <p className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
-                Additional references
-              </p>
-              <div className="mt-[12px] rounded-[12px] border-[1px] border-[#0000004d] bg-white px-[16px] py-[16px] text-[14px] md:px-[24px] md:py-[16px] md:text-[16px]">
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: formatAdditionalReferences(
-                      data.additionalReferences,
-                    ),
-                  }}
-                />
+              <div className="mt-3 flex items-center space-x-2 text-sm md:text-base">
+                <div className="text-neutral-500">
+                  Uploaded {formatDate(data.createdAt)}
+                </div>
+                <div className="select-none text-neutral-300">|</div>
+                <div className="text-neutral-500">{data.views} Views</div>
+                <div className="grow"></div>
+                <div>
+                  <ShareButton />
+                </div>
               </div>
-            </div>
+
+              <div className="mt-[20px] rounded-xl border-4 border-[#c7dfbd] bg-[#c7dfbd] md:mt-[42px]">
+                <div className="px-2 py-1.5 pb-2 text-sm font-bold uppercase tracking-[3.5px] text-[#5f7d54] md:text-lg">
+                  Question
+                </div>
+                <h2 className="rounded-b-xl bg-[#fff] px-4 py-3 text-[14px] font-medium leading-[24px] text-[#19372B] md:text-[20px] md:leading-[32px]">
+                  {data.question}
+                </h2>
+              </div>
+
+              {data?.audios &&
+                data?.audios.map((audio: Audio) => (
+                  <AudioPlayer key={audio._id} url={audio.url} />
+                ))}
+
+              <div className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
+                {data.author}
+              </div>
+
+              <div className="mt-[15px] whitespace-pre-wrap text-[15px] leading-7 md:text-[18px] md:leading-10">
+                {data.reply}
+              </div>
+
+              {data.additionalReferences && (
+                <div className="mt-[20px]">
+                  <p className="mt-[20px] text-sm font-bold uppercase leading-[24px] tracking-[4px] text-[#5f7d54] md:mt-[42px] md:text-[18px]">
+                    Additional references
+                  </p>
+                  <div className="mt-[12px] rounded-[12px] border-[1px] border-[#0000004d] bg-white px-[16px] py-[16px] text-[14px] md:px-[24px] md:py-[16px] md:text-[16px]">
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: formatAdditionalReferences(
+                          data.additionalReferences,
+                        ),
+                      }}
+                    />
+                  </div>
+                </div>
+              )}
+            </>
           )}
         </div>
       </div>
