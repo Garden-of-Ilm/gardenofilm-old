a276353788745289502fe0d300974d304ebc33aa
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2024-06-10 11:54:56 -0400

    Update create.tsx

--- frontend/src/pages/admin/fatwas/create.tsx
+++ frontend/src/pages/admin/fatwas/create.tsx
@@ -1,56 +1,53 @@
-import React, { useRef, useState } from "react";
-import axiosInstance from "@/lib/axios";
-import { useRouter } from "next/router";
-import { Audio } from "@/lib/definitions";
-import { FormProvider, useForm } from "react-hook-form";
-import AdminLayout from "@/components/admin-layout";
+import React, { FormEvent, useState } from "react";
+
 import Link from "next/link";
+import { useRouter } from "next/router";
 
+import { Audio } from "@/lib/definitions";
+import axiosInstance from "@/lib/axios";
+
+import AdminLayout from "@/components/admin-layout";
 import AudioUpload from "@/components/audio-upload";
-import RHFTextAreaField from "@/components/RHFTextAreaField";
-
 import { Button } from "@/components/ui/button";
-import { ChevronLeft, TriangleAlert } from "lucide-react";
 import { Label } from "@/components/ui/label";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { ChevronLeft, TriangleAlert } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function FatwaCreate() {
   const router = useRouter();
 
   const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
   const [fatwaAudios, setFatwaAudios] = useState<Audio[] | []>([]);
-  const defaultValues: any = {
-    author: "",
-    reply: "",
-    title: "",
-    question: "",
-    additionalReferences: "",
-    audios: [],
-  };
 
-  const methods = useForm({ defaultValues });
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-  const { handleSubmit } = methods;
+    const formData = new FormData(event.currentTarget);
 
-  const onSubmit = async (data: typeof defaultValues) => {
-    await handleCreate(data);
-  };
-
-  const submitBtnRef = useRef<HTMLButtonElement | null>(null);
-
-  const handleCreate = async (data: typeof defaultValues) => {
     axiosInstance
-      .post("/fatwas", data)
+      .post("/fatwas", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
       .then((res) => {
         router.push("/admin/fatwas");
       })
       .catch((err: any) => {
         setError(err?.response?.data?.message ?? "Something went wrong");
       });
-  };
-
-  if (loading) {
-    return;
   }
 
   return (
@@ -64,103 +61,115 @@ export default function FatwaCreate() {
           Back
         </Link>
       </div>
-      <div className="pb-10 pt-5">
-        <FormProvider {...methods}>
-          <form
-            className="mx-auto max-w-3xl px-4"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="text-lg font-semibold leading-none tracking-tight">
-              Upload new fatwa
+      <div className="px-4 pb-10 pt-5">
+        <form method="post" className="mx-auto max-w-3xl" onSubmit={onSubmit}>
+          <div className="text-lg font-semibold leading-none tracking-tight">
+            Upload new fatwa
+          </div>
+          {error && (
+            <div className="mt-4 flex w-full items-center justify-center rounded-md bg-red-100 py-3 text-base font-medium uppercase text-rose-600">
+              <TriangleAlert className="h-6 w-6" /> {error}
             </div>
-            {error && (
-              <div className="mt-4 flex w-full items-center justify-center rounded-md bg-red-100 py-3 text-base font-medium uppercase text-rose-600">
-                <TriangleAlert className="h-6 w-6" /> {error}
-              </div>
-            )}
+          )}
 
-            <div className="mt-4">
-              <Label>Author</Label>
-              <RHFTextAreaField
-                className="mt-1 w-full rounded-lg border border-gray-400 p-6"
-                name="author"
-                placeholder="Author"
-              />
-            </div>
+          <div className="mt-6 grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="title">
+              Title{" "}
+              <span className="font-normal text-zinc-400">(required)</span>
+            </Label>
+            <Input
+              type="text"
+              className="border-slate-400"
+              id="title"
+              name="title"
+              required
+            />
+          </div>
 
-            <div className="mt-4">
-              <Label>Title</Label>
-              <RHFTextAreaField
-                className={
-                  "mt-1 block w-full rounded-lg border border-gray-400 bg-white p-6 text-base text-gray-800 outline-none"
-                }
-                name="title"
-                placeholder="Title"
-              />
-            </div>
+          <div className="mt-6 grid w-full max-w-sm items-center gap-1.5">
+            <Label>
+              Author{" "}
+              <span className="font-normal text-zinc-400">(required)</span>
+            </Label>
+            <Select name="author" required>
+              <SelectTrigger className="w-[250px] border-slate-400">
+                <SelectValue placeholder="Select an author" />
+              </SelectTrigger>
+              <SelectContent className="border-slate-300">
+                <SelectGroup>
+                  <SelectLabel>Authors</SelectLabel>
+                  <SelectItem value="Sheikh Ibn Baz">Sheikh Ibn Baz</SelectItem>
+                  <SelectItem value="Sheikh Ibn Al-Uthaymeen">
+                    Sheikh Ibn Al-Uthaymeen
+                  </SelectItem>
+                  <SelectItem value="Sheikh Al-Albani">
+                    Sheikh Al-Albani
+                  </SelectItem>
+                  <SelectItem value="Sheikh Saleh Al-Fawzan">
+                    Sheikh Saleh Al-Fawzan
+                  </SelectItem>
+                  <SelectItem value="Sheikh Saleh Al-Luhaydan">
+                    Sheikh Saleh Al-Luhaydan
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
 
-            <div className="mt-4">
-              <Label>Question</Label>
-              <RHFTextAreaField
-                name="question"
-                className={
-                  "mt-1 block w-full rounded-lg border border-gray-400 bg-white p-6 text-base text-gray-800 outline-none"
-                }
-                placeholder="Question"
-              />
-            </div>
+          <div className="mt-6 grid w-full items-center gap-1.5">
+            <Label htmlFor="question">
+              Question{" "}
+              <span className="font-normal text-zinc-400">(required)</span>
+            </Label>
+            <Input
+              type="text"
+              className="border-slate-400"
+              id="question"
+              name="question"
+              required
+            />
+          </div>
 
-            <div className="mt-4">
-              <Label>Reply</Label>
-              <RHFTextAreaField
-                name="reply"
-                className={
-                  "mt-1 block w-full rounded-lg border border-gray-400 bg-white p-6 text-base text-gray-800 outline-none"
-                }
-                placeholder="Reply"
-              />
-            </div>
+          <div className="mt-6 grid w-full gap-1.5">
+            <Label htmlFor="reply">
+              Reply{" "}
+              <span className="font-normal text-zinc-400">(required)</span>
+            </Label>
+            <Textarea
+              className="border-slate-400"
+              id="reply"
+              name="reply"
+              rows={10}
+              required
+            />
+          </div>
 
-            <div className="mt-4">
-              <Label>Category</Label>
-              <RHFTextAreaField
-                name="category"
-                className={
-                  "mt-1 block w-full rounded-lg border border-gray-400 bg-white p-6 text-base text-gray-800 outline-none"
-                }
-                placeholder="Category"
-              />
-            </div>
+          <div className="mt-6 grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="category">Category</Label>
+            <Input
+              type="text"
+              className="border-slate-400"
+              id="category"
+              name="category"
+            />
+          </div>
+          <div className="mt-6 grid w-full gap-1.5">
+            <Label htmlFor="additionalReferences">Additional References</Label>
+            <Textarea
+              className="border-slate-400"
+              id="additionalReferences"
+              name="additionalReferences"
+            />
+          </div>
 
-            <div className="mt-4">
-              <Label>Additional References</Label>
-              <RHFTextAreaField
-                name="additionalReferences"
-                className={
-                  "mt-1 block w-full rounded-lg border border-gray-400 bg-white p-6 text-base text-gray-800 outline-none"
-                }
-                placeholder="Additional References"
-              />
-            </div>
+          <div className="mt-6">
+            <AudioUpload audios={fatwaAudios} />
+          </div>
 
-            <div className="mt-4">
-              <AudioUpload audios={fatwaAudios} />
-            </div>
-            <button type="submit" className="hidden" ref={submitBtnRef} />
-            <div className="mt-8">
-              <Button
-                type="button"
-                onClick={() => {
-                  if (submitBtnRef?.current) {
-                    submitBtnRef.current.click();
-                  }
-                }}
-              >
-                Submit
-              </Button>
-            </div>
-          </form>
-        </FormProvider>
+          <Button className="mt-8" type="submit">
+            Submit
+          </Button>
+        </form>
       </div>
     </AdminLayout>
   );
